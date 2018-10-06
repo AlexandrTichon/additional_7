af7f5823ed6d97906873da086ea1a8fce037d10d
Alexandr Tichon <tsikhan.p@gmail.com>
2018-10-06 22:15:19 +0300

    Sory

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+  var solution = [];
+  var steps = 0;
+  inputSol(matrix);
+  console.log(matrix);
+  console.log(solution);
+
+function inputSol(in_val) {
+  steps = 0;
+  var suggest = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  for ( var i=0; i<9; i++) {
+      solution[i] = [];
+      for ( var j=0; j<9; j++ ) {
+          if ( in_val[i][j] ) {
+              solution[i][j] = [in_val[i][j], 'default', []];
+          }
+          else {
+              solution[i][j] = [0, 'nothing', suggest];
+          }
+      }
+  }
+}
 }
